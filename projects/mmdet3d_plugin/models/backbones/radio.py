134cc969510a1a83370f86722d315aa1e8cc0d3b
livanoff <drfrily@gmail.com>
2025-05-09 13:46:38 +0300

    add radio

---
+++ projects/mmdet3d_plugin/models/backbones/radio.py
@@ -0,0 +1,25 @@
+import torch
+
+from mmdet.models.builder import BACKBONES
+from mmcv.runner.base_module import BaseModule
+
+@BACKBONES.register_module()
+class RADIO(BaseModule):
+    def __init__(self,
+                 model_version="radio_v2.5-b",
+                 img_size=[480, 800],
+                 freeze=None
+                 ):
+        super(RADIO, self).__init__()
+        self.model = torch.hub.load('NVlabs/RADIO', 'radio_model', version=model_version, progress=True, skip_validation=True)
+        if "e-radio" in model_version:
+            self.model.model.set_optimal_window_size(img_size)
+        
+        #self._freeze_stages()
+        
+    def forward(self, x):
+        x.div_(255.0)
+        # import ipdb; ipdb.set_trace()
+        summary, spatial_features = self.model(x, feature_fmt='NCHW')
+        assert spatial_features.ndim == 4
+        return spatial_features
